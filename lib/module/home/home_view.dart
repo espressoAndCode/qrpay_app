f6c39f2c786b355a9133c268e3301b73a02ea5f0
Aarav Chandra <aaravchandra02@gmail.com>
2020-12-06 11:03:31 -0500

    converted to stateful

--- lib/module/home/home_view.dart
+++ lib/module/home/home_view.dart
@@ -2,41 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:qrpay_app/services/auth.dart';
 import 'package:qrpay_app/widget/drawer.dart';
 
-class HomePage extends StatelessWidget {
-
+class HomePage extends StatefulWidget {
   static const String routeName = '/home';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("QRPAY Touch-Free"),
-          actions: <Widget>[
-            FlatButton.icon(
-                icon: Icon(Icons.person),
-                onPressed: () async {
-                  await _auth.signOut();
-                },
-                label: Text('Logout'))
-          ]
-        ),
+        appBar: AppBar(title: Text("QRPAY Touch-Free"), actions: <Widget>[
+          FlatButton.icon(
+              icon: Icon(Icons.person),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              label: Text('Logout'))
+        ]),
         drawer: AppDrawer(),
         body: new Center(
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Container(
-                child: new Image.asset(
-                  'res/images/qplogo.png',
-                  width: MediaQuery.of(context).size.width * .9,
-                  fit: BoxFit.cover,
-                ),
+            child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              child: new Image.asset(
+                'res/images/qplogo.png',
+                width: MediaQuery.of(context).size.width * .9,
+                fit: BoxFit.cover,
               ),
-            ],
-          )
-        )
-    );
+            ),
+          ],
+        )));
   }
-
 }
