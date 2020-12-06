6114a9e278bfc95a32c19d6f2f7699f3f8717fcc
Aarav Chandra <aaravchandra02@gmail.com>
2020-12-06 10:39:36 -0500

    formatting changes

--- lib/module/wrapper.dart
+++ lib/module/wrapper.dart
@@ -3,17 +3,17 @@ import 'package:provider/provider.dart';
 import 'package:qrpay_app/module/authenticate/authenticate_view.dart';
 import 'home/home_view.dart';
 import 'package:qrpay_app/module/account/user.dart';
-    
+
 class Wrapper extends StatelessWidget {
   static const String routeName = '/wrapper';
-    @override
-    Widget build(BuildContext context) {
 
-      final user = Provider.of<User>(context);
-      if (user == null){
-        return Authenticate();
-      } else {
-        return HomePage();
-      }
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return HomePage();
     }
   }
+}
