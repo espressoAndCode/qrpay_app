3c6356c24eaefcb023a60785addde0f2c3922622
Aarav Chandra <aaravchandra02@gmail.com>
2020-12-05 23:48:14 -0500

    Edited about_me + working floating button

--- lib/module/about us/about_us_view.dart
+++ lib/module/about us/about_us_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qrpay_app/widget/drawer.dart';
+import 'package:qrpay_app/module/splash/splash_view.dart';
 
 class AboutUsPage extends StatelessWidget {
   static const String routeName = '/about_us';
@@ -8,36 +9,70 @@ class AboutUsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("About Us"),
+        title: Center(child: Text("About Us")),
+        actions: [
+          IconButton(
+            icon: Image.asset('res/images/qplogo.png'),
+            onPressed: () {},
+          ),
+        ],
         elevation: 30.0,
       ),
       drawer: AppDrawer(),
       body: Center(
-          child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             Container(
               // alignment: Alignment.center,
-              padding: EdgeInsets.fromLTRB(20, 60, 20, 20),
+              padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
               child: Text(
-                "QRPAY is a mobile payment service that allows users to make touch-free purchases in stores and restaurants just by attaching a payment method and scanning a QR Code.",
+                "About QRPAY",
                 style: TextStyle(
-                  fontSize: 27,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
                 ),
               ),
             ),
             Container(
-              padding: EdgeInsets.fromLTRB(20, 60, 20, 20),
+              padding: EdgeInsets.fromLTRB(30, 0, 20, 30),
+              child: Text(
+                "QRPAY is a mobile payment service that allows users to make touch-free purchases in stores and restaurants just by attachig a payment method and scanning a QR code.",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 0, 20, 30),
               child: Text(
                 "Payments can also be collected from others by creating unique QR Codes that others can scan to pay you.",
                 style: TextStyle(
-                  fontSize: 27,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ])),
-
+          ],
+        ),
+      ),
       //
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Click it to return to the Homepage',
+        hoverElevation: 50,
+        onPressed: () {
+          print("Going back to homepage");
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => SplashPage(),
+            ),
+          );
+        },
+        child: Icon(Icons.home),
+      ),
     );
   }
 }
