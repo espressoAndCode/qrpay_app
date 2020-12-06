d24a87f614b7455e35ca1abfecdc68af212e4f12
Aarav Chandra <aaravchandra02@gmail.com>
2020-12-06 00:30:45 -0500

    added home floating button

--- lib/module/receipts/receipts_list_view.dart
+++ lib/module/receipts/receipts_list_view.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:qrpay_app/widget/drawer.dart';
 import 'receipt_data.dart';
+import 'package:qrpay_app/module/wrapper.dart';
 
 class ReceiptsPage extends StatelessWidget {
-
-
   static const String routeName = '/receipts';
-
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +30,20 @@ class ReceiptsPage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Click it to return to the Homepage',
+        hoverElevation: 50,
+        onPressed: () {
+          print("Going back to homepage");
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => Wrapper(),
+            ),
+          );
+        },
+        child: Icon(Icons.home),
+      ),
     );
   }
 }
-
